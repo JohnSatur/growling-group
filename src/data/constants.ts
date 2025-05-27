54e7a4c53bb8b0f445fcc0eaf9e800af4b8952bc
John Satur <animashorse3@yahoo.com.mx>
2025-05-27 16:32:00 -0600

    Estructura inicial del proyecto

---
+++ src/data/constants.ts
@@ -0,0 +1,33 @@
+import ogImg from '@images/logo-gradiente-mini.png';
+
+export const SITE = {
+  title: 'Growling Group',
+  tagline: 'Seguridad industrial',
+  description: 'Grupo de seguridad industrial que ofrece soluciones integrales para la protección de personas y equipos. Somos una empresa comprometida con la seguridad y el bienestar de nuestros clientes.',
+  descriptionShort: 'Grupo de seguridad industrial que ofrece soluciones integrales para la protección de personas y equipos.',
+  url: 'https://growlingroup.com',
+  author: 'Juan Carlos Marín',
+}
+
+export const SEO = {
+  title: SITE.title,
+  description: SITE.description,
+  stucturedData: {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    inLanguage: 'es-MX',
+    '@id': SITE.url,
+    url: SITE.url,
+    name: SITE.title,
+    description: SITE.description,
+  }
+}
+
+export const OG = {
+  locale: 'es-MX',
+  type: 'website',
+  url: SITE.url,
+  title: `${SITE.title} - ${SITE.tagline}`,
+  description: SITE.description,
+  image: ogImg,
+}
